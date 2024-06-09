f49d0501472ceda68d7b5892e6159624de2be41a
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2024-06-09 10:47:41 +0600

    Update JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolChars = "!-$^+";
+const spaceChars = " ";
+
